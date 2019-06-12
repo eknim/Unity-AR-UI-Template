0bc599c2eda7fd51dbe9ad6f7418f97b4ae7bee9
eeknim <kyoroki@gmail.com>
2019-06-12 20:53:05 +0900

    add csv reading scripts and test example

---
+++ Assets/Scripts/CSVReadingTest.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CSVReadingTest : MonoBehaviour
+{
+
+    public int _exp = 0;
+
+    void Start()
+    {
+        List<Dictionary<string, object>> data = CSVReader.Read("exp");
+
+        for (var i = 0; i < data.Count; i++)
+        {
+            Debug.Log("index " + (i).ToString() + " : " + data[i]["EXP"] + " " + data[i]["Bonus"] + " " + data[i]["Text"]);
+            
+        }
+
+        _exp = (int)data[0]["EXP"];
+        //Debug.Log(_exp);
+    }
+}
